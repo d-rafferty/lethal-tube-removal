d9c3716b2194197662bde740368d882ca7e11ae2
Derek <derekrafferty@protonmail.com>
2024-02-20 17:52:55 -0500

    Initial commit

---
+++ Patches/TubeRemovalPatch.cs
@@ -0,0 +1,53 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using BepInEx;
+using System.Runtime.CompilerServices;
+using Object = UnityEngine.Object;
+using static LethalTubeRemoval.TubeRemoval;
+
+//NOTES
+//Environment/HangarShip/Bunkbeds
+//Environment/HangarShip/FileCabinet
+//Environment/HangarShip/ScavengerModelSuitParts/Circle.002
+
+namespace LethalTubeRemoval.Patches
+{
+    [HarmonyPatch(typeof(GameObject))]
+    internal class TubeRemovalPatch
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(StartOfRound), "Start")]
+        static void TubeRemove()
+        {
+            GameObject tube = GameObject.Find("Environment/HangarShip/BezierCurve");
+            GameObject beds = GameObject.Find("Environment/HangarShip/Bunkbeds");
+            GameObject cabinet = GameObject.Find("Environment/HangarShip/FileCabinet");
+            GameObject tank = GameObject.Find("Environment/HangarShip/ScavengerModelSuitParts/Circle.002");
+
+            if (Config.deleteTube.Value)
+            {
+                GameObject.Destroy(tube);
+            }
+
+            if (Config.deleteBunkbeds.Value)
+            {
+                GameObject.Destroy(beds);
+            }
+
+            if (Config.deleteFileCabinets.Value)
+            {
+                GameObject.Destroy(cabinet);
+            }
+
+            if (Config.deleteOxygenTank.Value)
+            {
+                GameObject.Destroy(tank);
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,97 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using LethalConfig.ConfigItems;
+using LethalConfig;
+using LethalTubeRemoval.Patches;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using System.Security.Policy;
+
+namespace LethalTubeRemoval
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    [BepInDependency("ainavt.lc.lethalconfig")]
+    public class TubeRemoval : BaseUnityPlugin
+    {
+        private const string modGUID = "Hamster.LethalTubeRemoval";
+        private const string modName = "Lethal Tube Removal";
+        private const string modVersion = "1.0.0";
+
+        public static new Config MyConfig { get; internal set; }
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static TubeRemoval Instance;
+
+        internal ManualLogSource mls;
+
+        public class Config
+        {
+            public static ConfigEntry<bool> deleteTube;
+            public static ConfigEntry<bool> deleteBunkbeds;
+            public static ConfigEntry<bool> deleteFileCabinets;
+            public static ConfigEntry<bool> deleteOxygenTank;
+            public Config(ConfigFile cfg)
+            {
+                deleteTube = cfg.Bind(
+                   "General",
+                   "Tube",
+                   true,
+                   "Deletes the tube on the floor"
+               );
+                var tubeToggle = new BoolCheckBoxConfigItem(deleteTube, requiresRestart: false);
+                LethalConfigManager.AddConfigItem(tubeToggle);
+
+                deleteBunkbeds = cfg.Bind(
+                    "General",
+                    "BunkBeds",
+                    false,
+                    "Deletes the bunkbeds"
+                );
+                var bedToggle = new BoolCheckBoxConfigItem(deleteBunkbeds, requiresRestart: false);
+                LethalConfigManager.AddConfigItem(bedToggle);
+
+                deleteFileCabinets = cfg.Bind(
+                    "General",
+                    "File Cabinets",
+                    false,
+                    "Deletes the file cabinets"
+                );
+                var fileCabinetsToggle = new BoolCheckBoxConfigItem(deleteFileCabinets, requiresRestart: false);
+                LethalConfigManager.AddConfigItem(fileCabinetsToggle);
+
+                deleteOxygenTank = cfg.Bind(
+                    "General",
+                    "Oxygen Tank",
+                    false,
+                    "Deletes the oxygen tank"
+                );
+                var oxygenTankToggle = new BoolCheckBoxConfigItem(deleteOxygenTank, requiresRestart: false);
+                LethalConfigManager.AddConfigItem(oxygenTankToggle);
+            }
+        }
+
+        void Awake()
+        {
+            if (Instance = null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            mls.LogInfo("Mod is woked");
+
+            MyConfig = new(base.Config);
+
+            harmony.PatchAll(typeof(TubeRemoval));
+            harmony.PatchAll(typeof(TubeRemovalPatch));
+        }
+    }
+
+}
