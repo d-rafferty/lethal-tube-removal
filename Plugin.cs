7e0935b260a580665b30c41a0f612d9c84cf2316
Derek <derekrafferty@protonmail.com>
2024-05-31 13:53:45 -0400

    version change

--- Plugin.cs
+++ Plugin.cs
@@ -11,7 +11,7 @@ public class TubeRemoval : BaseUnityPlugin
 {
     private const string modGUID = "Hamster.LethalTubeRemoval";
     private const string modName = "Lethal Tube Removal";
-    private const string modVersion = "1.7.4";
+    private const string modVersion = "1.7.5";
 
     private static TubeRemoval? Instance;
 
